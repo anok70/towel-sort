27404372d3ff34dfff77909396da8909f97bf66b
anok70 <anok70@mail.ru>
2022-08-07 19:42:48 +0300

    completed tas towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
-// You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+let arrInt = [];
+
+if (arguments.length){    //   когда есть параметры передаваемые в функцию
+  if (matrix.length!== 0) {
+    for (i = 0; i < matrix.length; i++){
+      if (i%2) matrix[i].reverse(); 
+      for (j = 0; j < matrix[i].length; j++) arrInt.push(matrix[i][j]);
+    }
+  }
+}
+return arrInt; 
 }
